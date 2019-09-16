4e13a18ca0bc20fb3d3003f833bbf4f8a8b78955
daid <daid303@gmail.com>
2019-09-16 10:41:48 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -229,7 +229,7 @@ int main(int argc, char** argv)
     sp::P<sp::Engine> engine = new sp::Engine();
     
     sp::gui::Theme::loadTheme("default", "gui/theme/basic.theme.txt");
-    new sp::gui::Scene(sp::Vector2d(256, 192), sp::gui::Scene::Direction::Horizontal);
+    new sp::gui::Scene(sp::Vector2d(256, 192));
 
     sp::P<sp::SceneGraphicsLayer> scene_layer = new sp::SceneGraphicsLayer(1);
     scene_layer->addRenderPass(new sp::BasicNodeRenderPass());
